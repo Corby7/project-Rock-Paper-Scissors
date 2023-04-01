9c88fed62331c37e44044297794312a4d8d29175
Corbijn Bulsink <corbijn.bulsink@hotmail.com>
2023-04-01 16:16:21 -0700

    added first function: getComputerChoice

---
+++ index.js
@@ -0,0 +1,14 @@
+const choices = ["Rock","Paper","Scissor"];
+
+function playRound(playerSelection, computerSelection) {
+    if playerSelection = computerSelection
+}
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice;
+
+console.log(getComputerChoice());
