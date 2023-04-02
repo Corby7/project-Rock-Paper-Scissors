141a6eb165e3a5edc6a5457ad093c38c14bc6990
Corbijn Bulsink <corbijn.bulsink@hotmail.com>
2023-04-01 18:39:27 -0700

    added playRound function

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const choices = ["Rock","Paper","Scissor"];
+const choices = ["rock", "paper", "scissors"];
 
-function playRound(playerSelection, computerSelection) {
-    if playerSelection = computerSelection
-}
-
+//get computer choice
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice;
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return ("It's a draw!");
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+       return "You Lose! Paper beats Rock";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beats Paper";
+    }
+}
 
-console.log(getComputerChoice());
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+    
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
